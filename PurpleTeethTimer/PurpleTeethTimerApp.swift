28af3fc4117f62457d9decb187e02b939d78541f
jv1059 <jv1059@wildcats.unh.edu>
2021-04-29 21:19:15 -0400

    Initial Commit

---
+++ PurpleTeethTimer/PurpleTeethTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurpleTeethTimerApp.swift
+//  PurpleTeethTimer
+//
+//  Created by Jakob Velle on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct PurpleTeethTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
